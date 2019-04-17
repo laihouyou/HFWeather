5150f05d8f4d8f85bb5dd5d2d1499b81f6ba4ea8
laihouyou <laihouyou163@163.com>
2019-04-17 19:04:05 +0800

    1添加了搜索页面

---
+++ weather/src/main/java/com/gddst/lhy/weather/fragment/CitySearchFragment.java
@@ -0,0 +1,30 @@
+package com.gddst.lhy.weather.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gddst.app.lib_common.base.BaseFragment;
+
+public class CitySearchFragment extends BaseFragment {
+    @Override
+    protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void lazyLoad() {
+
+    }
+}
