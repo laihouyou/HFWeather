8aba4e746adc74cd860b1a0b08965f3ba4c6fa8e
lhy <laihouyou163@163.com>
2019-12-02 18:12:31 +0800

    1.玩安卓添加 数据

---
+++ wanandroid/src/main/java/com/gddst/webapp/wanandroid/ui/WanAndroidMainActivity.kt
@@ -0,0 +1,68 @@
+package com.gddst.webapp.wanandroid.ui
+
+import android.os.Bundle
+import android.view.View.OVER_SCROLL_NEVER
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.gddst.app.lib_common.commonAdapter.recycleView.CommonRecycleViewAdapter
+import com.gddst.webapp.wanandroid.R
+import com.github.jdsjlzx.ItemDecoration.DividerDecoration
+import com.github.jdsjlzx.recyclerview.ProgressStyle
+import kotlinx.android.synthetic.main.activity_wan_android_main.*
+
+class WanAndroidMainActivity : AppCompatActivity() {
+
+    var adapter: CommonRecycleViewAdapter<String>?=null
+    var mAdapter: CommonRecycleViewAdapter<String>?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wan_android_main)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+
+        adapter=CommonRecycleViewAdapter<String>(this,)
+
+        recyclerView.adapter=mAdapter
+
+        recyclerView.layoutManager= LinearLayoutManager(this)
+        recyclerView.overScrollMode=OVER_SCROLL_NEVER
+
+        recyclerView.addItemDecoration(getItemDecoration())
+
+        recyclerView.setRefreshProgressStyle(ProgressStyle.LineSpinFadeLoader)
+        recyclerView.setArrowImageView(R.drawable.ic_pulltorefresh_arrow)
+        recyclerView.setLoadingMoreProgressStyle(ProgressStyle.BallSpinFadeLoader)
+
+        recyclerView.setLoadMoreEnabled(true)
+
+        recyclerView.setOnRefreshListener {
+
+        }
+        recyclerView.setOnLoadMoreListener {
+
+        }
+
+        //设置头部加载颜色
+        recyclerView.setHeaderViewColor(R.color.colorAccent, R.color.dark, android.R.color.white)
+        //设置底部加载颜色
+        recyclerView.setFooterViewColor(R.color.colorAccent, R.color.dark, android.R.color.white)
+        //设置底部加载文字提示
+        recyclerView.setFooterViewHint("拼命加载中", "我是有底线的", "网络不给力啊，点击再试一次吧")
+
+        recyclerView.refresh()
+    }
+
+    private fun getItemDecoration(): RecyclerView.ItemDecoration {
+        return DividerDecoration.Builder(this)
+                .setHeight(R.dimen.dp_4)
+                .setPadding(R.dimen.dp_4)
+                .setColorResource(R.color.split)
+                .build()
+    }
+
+
+}
---
+++ wanandroid/src/main/java/com/gddst/webapp/wanandroid/vo/ArticleVo.kt
@@ -0,0 +1,65 @@
+package com.gddst.webapp.wanandroid.vo
+
+class ArticleVo {
+    /**
+     * apkLink :
+     * audit : 1
+     * author :
+     * chapterId : 502
+     * chapterName : 自助
+     * collect : false
+     * courseId : 13
+     * desc :
+     * envelopePic :
+     * fresh : false
+     * id : 10365
+     * link : https://mp.weixin.qq.com/s/d9lx8iSGRabeuuYYVz-z1Q
+     * niceDate : 2019-11-18
+     * niceShareDate : 2019-11-18
+     * origin :
+     * prefix :
+     * projectLink :
+     * publishTime : 1574071117000
+     * selfVisible : 0
+     * shareDate : 1574071117000
+     * shareUser : chendroid
+     * superChapterId : 494
+     * superChapterName : 广场Tab
+     * tags : []
+     * title : Plaid 开源库学习
+     * type : 0
+     * userId : 7075
+     * visible : 1
+     * zan : 0
+     */
+
+    var apkLink: String? = null
+    var audit: Int = 0
+    var author: String? = null
+    var chapterId: Int = 0
+    var chapterName: String? = null
+    var isCollect: Boolean = false
+    var courseId: Int = 0
+    var desc: String? = null
+    var envelopePic: String? = null
+    var isFresh: Boolean = false
+    var id: Int = 0
+    var link: String? = null
+    var niceDate: String? = null
+    var niceShareDate: String? = null
+    var origin: String? = null
+    var prefix: String? = null
+    var projectLink: String? = null
+    var publishTime: Long = 0
+    var selfVisible: Int = 0
+    var shareDate: Long = 0
+    var shareUser: String? = null
+    var superChapterId: Int = 0
+    var superChapterName: String? = null
+    var title: String? = null
+    var type: Int = 0
+    var userId: Int = 0
+    var visible: Int = 0
+    var zan: Int = 0
+    var tags: List<*>? = null
+}
